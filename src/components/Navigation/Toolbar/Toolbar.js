b78e2aa3eda3718f161222074b204adf2eb2461b
Philipp Albrecht <philippbalbrecht@gmail.com>
2018-08-26 12:54:00 -0700

    More about Responsive Adjustments

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import classes from './Toolbar.css'
 import Logo from '../../Logo/Logo'
-import NavigationItem from '../NavigationItems/NavigationItems'
+import NavigationItems from '../NavigationItems/NavigationItems'
 
 const toolbar = (props) => (
   <header className={classes.Toolbar}>
     <div>MENUE</div>
     <Logo height="80%"/>
-    <nav>
-      <NavigationItem/>
+    <nav className={classes.DesktopOnly}>
+      <NavigationItems/>
     </nav>
   </header>
 );
